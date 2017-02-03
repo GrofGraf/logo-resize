67a935df6939c9f1592add355656dc0c41045693
Admin <Admin>
2017-02-03 10:45:21 +0100

    rewritten

--- logo-resize.js
+++ logo-resize.js
@@ -3,7 +3,7 @@ document.addEventListener("scroll", navbarScroll);
 document.addEventListener("touchmove", navbarScroll);
 function navbarScroll(){
 	var logo = document.getElementById("logo");
-	position = window.pageYOffset || document.body.scrollTop;
+	var position = window.pageYOffset || document.body.scrollTop;
 	if(position>60 && !logo.classList.contains("shrink")){
 		logo.classList.add("shrink");
 	}
